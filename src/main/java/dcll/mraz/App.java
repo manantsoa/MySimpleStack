a21d56c2c612b2a506d91567c3e0e5fc7edbc82f
manantsoa <manantsoa.razana@gmail.com>
2015-02-09 14:53:27 +0100

    creation of the MySimpleStack project

---
+++ src/main/java/dcll/mraz/App.java
@@ -0,0 +1,13 @@
+package dcll.mraz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
